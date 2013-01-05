040616c444bab41bfd21cb488e7e95bbb716cfe1
Patrick Bougie <patrick@bougie.ca>
2013-01-05 16:02:16 -0500

    Added Google Analytics tracking code using Rack Middleware.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source :rubygems
 
 # http://middlemanapp.com/
 gem "middleman"
+
+# https://github.com/ambethia/rack-google_analytics
+gem "rack-google_analytics"
--- config.rb
+++ config.rb
@@ -5,4 +5,6 @@ set :images_dir, 'images'
 configure :build do
   activate :minify_css
   activate :minify_javascript
+
+  use Rack::GoogleAnalytics, :web_property_id => ENV['GOOGLE_ANALYTICS']
 end
