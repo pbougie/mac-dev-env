ed14b0cb12a8f19c968d245d4fa3bc81202e1bdc
Patrick Bougie <patrick@bougie.ca>
2013-01-05 16:02:28 -0500

    Added Gauges tracking code using Rack Middleware.

--- Gemfile
+++ Gemfile
@@ -5,3 +5,6 @@ gem "middleman"
 
 # https://github.com/ambethia/rack-google_analytics
 gem "rack-google_analytics"
+
+# https://github.com/thunderboltlabs/rack-gauges
+gem "rack-gauges"
--- config.rb
+++ config.rb
@@ -7,4 +7,5 @@ configure :build do
   activate :minify_javascript
 
   use Rack::GoogleAnalytics, :web_property_id => ENV['GOOGLE_ANALYTICS']
+  use Rack::Gauges, :tracker => ENV['GAUGES']
 end
